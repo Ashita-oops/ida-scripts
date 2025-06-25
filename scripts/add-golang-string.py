1f46f9e740d9b90efd7781e93b810138148a5474
Ashita-oops <ashitabridget@gmail.com>
2025-06-25 03:32:02 -0700

    chore: update for better heuristic

--- scripts/add-golang-string.py
+++ scripts/add-golang-string.py
@@ -3,8 +3,16 @@ import ida_bytes
 import idaapi
 import ida_search
 import idc
+import ida_ida
+
+'''
+NOTE: Handle for 64-bit only :v
+'''
 
 def create_string(ea, strlen):
+    if strlen == None or strlen < 0:
+        return
+
     str_value = ida_bytes.get_strlit_contents(ea, strlen, idaapi.STRTYPE_C)
     if str_value == None:
         return
@@ -29,8 +37,42 @@ def str2int(txt: str):
 def get_ea_by_name(label: str):
     return idc.get_name_ea_simple(label)
 
-def hotkey_pressed():
-    ea = idaapi.get_screen_ea()
+def is_rodata_addr(ea):
+    if idc.get_segm_start(ea) == idc.BADADDR:
+        return False
+    return idc.get_segm_name(ea) == '.rodata' or idc.get_segm_name(ea) == '.data'
+
+def is_code_addr(ea):
+    if idc.get_segm_start(ea) == idc.BADADDR:
+        return False
+    return idc.get_segm_name(ea) == '.text'
+
+def is_small_int(val):
+    return 0 <= val < 2**32
+
+def handle_ea_in_rodata(ea):
+    wordlen = 8
+    if ida_ida.inf_is_32bit_exactly():
+        wordlen = 4
+
+    probable_addr = ida_bytes.get_bytes(ea, wordlen)
+    probable_addr = int.from_bytes(probable_addr, 'little')
+    probable_len  = ida_bytes.get_bytes(ea+wordlen, wordlen)
+    probable_len  = int.from_bytes(probable_len, 'little')
+
+    if not is_rodata_addr(probable_addr) or not is_small_int(probable_len):
+        length = ida_kernwin.ask_long(-1, "Detect @ 0x%x as start of a string.\n Please manually add length for string:" % ea)
+        if length != None:
+            create_string(ea, length)
+        return
+
+    todo_str = ida_bytes.get_strlit_contents(probable_addr, probable_len, idaapi.STRTYPE_C)
+
+    length = ida_kernwin.ask_long(probable_len, "Detect @ 0x%x as pointer to string:\n%s\nPlease confirm length of string:" % (probable_addr, todo_str))
+    if length != None:
+        create_string(ea, length)
+
+def handle_ea_in_code(ea):
     prev_ea = ida_search.find_code(ea, ida_search.SEARCH_UP)
     next_ea = ida_search.find_code(ea, ida_search.SEARCH_DOWN)
 
@@ -49,7 +91,8 @@ def hotkey_pressed():
     elif next_op != None:
         length = next_op
     else:
-        ida_kernwin.warning("Cannot find length data about string")
+        length = ida_kernwin.ask_long(-1, "Cannot find length data about string.\nDo you want to manually add length?")
+        create_string(curr_op, length)
         return
 
     todo_str = ida_bytes.get_strlit_contents(curr_op, length, idaapi.STRTYPE_C)
@@ -57,11 +100,19 @@ def hotkey_pressed():
         ida_kernwin.warning("Cannot find string at 0x%x" % curr_op)
         return
     
-    user_input = ida_kernwin.ask_yn(0, "Do you want to make this string: %s" % todo_str)
-    if user_input == 0:
-        return
+    length = ida_kernwin.ask_long(length, "Detect @ 0x%x as pointer to string:\n%s\nPlease confirm length of string:" % (curr_op, todo_str))
+    if length != None:
+        create_string(ea, length)
     
     create_string(curr_op, length)
+
+
+def hotkey_pressed():
+    ea = idaapi.get_screen_ea()
+    if is_rodata_addr(ea):
+        handle_ea_in_rodata(ea)
+    elif is_code_addr(ea):
+        handle_ea_in_code(ea)
 
 try:
     hotkey_ctx
